b74f679305de9c13b2c17bb64036acb26229ba60
Ghd <gracia.brueckmann@unibe.ch>
2026-07-20 17:44:03 +0200

    install the tidyverse instead of just ggplot2, stringr but later loading the tidyverse within scripts.

--- 02_rscripts/install.R
+++ 02_rscripts/install.R
@@ -1,11 +1,12 @@
 ### install packages
 
 
-packs <- c('dataverse', 'causaldata', 'data.table', 'estimatr', 'evaluate', 'fixest', 'ggplot2',
+packs <- c('dataverse', 'causaldata', 'tidyverse',
+           'data.table', 'estimatr', 'evaluate', 'fixest', 
            'haven', 
            #'lfe', #Package which is only available in source form, and may need compilation of C/C++/Fortran: ‘lfe’
             'magrittr', 'modelsummary', 'openssl', 'ragg',
-           'softbib', 'stringr', 'this.path', 'tinytable', 'xfun' )
+           'softbib',  'this.path', 'tinytable', 'xfun' )
 
 options(repos = c(CRAN = "https://packagemanager.posit.co/cran/2026-07-17"))
 
